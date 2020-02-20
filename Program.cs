dfc88b78b97a786b614fbe218bd1e0be82b0b759
Chris Gaus <chrisgaus@gmail.com>
2020-02-20 11:54:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csc565projectServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
